7cd6b702d8a89470f3b1fe9d69af2edf24203df7
ericksgmes <erick.dicksan@gmail.com>
2025-05-08 23:59:54 -0300

    sixth commit sorry

--- src/main/java/com/pw3/application/conserto/patch/RegistraSaidaService.java
+++ src/main/java/com/pw3/application/conserto/patch/RegistraSaidaService.java
@@ -1,5 +1,6 @@
 package com.pw3.application.conserto.patch;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -12,6 +13,8 @@ public interface RegistraSaidaService {
 
     record RequestModel(
             Long id,
+
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
             Date dataSaida
     ) { }
 }
